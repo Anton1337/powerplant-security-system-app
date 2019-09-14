c7750ac1ff854f31c1f8dd8d0e59caff3e3d8b68
Anton Andrésen <anton_real_madrid@icloud.com>
2019-09-14 09:07:49 +0200

    File system organized

---
+++ app/constants/Colors.js
@@ -0,0 +1,5 @@
+// Put color constants here.
+export default {
+  primary: '#C2185B', // Change these later (& add more?)
+  accent: 'FFC107',
+};
---
+++ app/data/dummy-data.js
@@ -0,0 +1,1 @@
+// Put test data here to prepare UI for actual data.
